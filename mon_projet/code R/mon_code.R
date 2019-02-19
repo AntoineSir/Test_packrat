8b425ce4d6c9ff326128484ddbaab72e26de0d9c
Antoine Sireyjol <antoine.sireyjol@gmail.com>
2019-02-19 18:33:24 +0100

    test avec ajout de microbenchmark

--- mon_projet/code R/mon_code.R
+++ mon_projet/code R/mon_code.R
@@ -1,5 +1,7 @@
 library(dplyr)
-
+library(microbenchmark)
 
 df <- data.frame(a = c(1:100), b = sample(50), c = rnorm(100), replace = TRUE)
 df %>% as_tibble() %>% group_by(b) %>% summarise(moy = mean(c)) -> out
+
+microbenchmark(times = 5, df %>% as_tibble() %>% group_by(b) %>% summarise(moy = mean(c)) )

adc59f48b14423aa3f5ba7c8fe04fafe4e7f4bee
antoine <antoine.sireyjol@gmail.com>
2019-02-19 18:41:01 +0100

    ajout de data.table depuis un poste sous linux

--- mon_projet/code R/mon_code.R
+++ mon_projet/code R/mon_code.R
@@ -1,7 +1,10 @@
 library(dplyr)
 library(microbenchmark)
+library(data.table)
 
 df <- data.frame(a = c(1:100), b = sample(50), c = rnorm(100), replace = TRUE)
 df %>% as_tibble() %>% group_by(b) %>% summarise(moy = mean(c)) -> out
 
 microbenchmark(times = 5, df %>% as_tibble() %>% group_by(b) %>% summarise(moy = mean(c)) )
+
+dt <- as.data.table(df)

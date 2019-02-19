b36c90679b9d16b96c83d5a0feb979a745855bb5
Antoine Sireyjol <antoine.sireyjol@gmail.com>
2019-02-19 18:13:03 +0100

    nouveau test sur packrat

--- mon_projet/code R/mon_code.R
+++ mon_projet/code R/mon_code.R
@@ -1,6 +1,5 @@
 library(dplyr)
 
+
 df <- data.frame(a = c(1:100), b = sample(50), c = rnorm(100), replace = TRUE)
 df %>% as_tibble() %>% group_by(b) %>% summarise(moy = mean(c)) -> out
-
-write.table(out, file = "output/moyenne.txt")
--- mon_projet/packages_download.R
+++ mon_projet/packages_download.R
@@ -1,3 +1,3 @@
 install.packages("dplyr")
-
+packrat::status()
 packrat::snapshot()

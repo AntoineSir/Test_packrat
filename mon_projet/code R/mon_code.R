f1841efee4305c7851e21d5e94d15be2f3e74919
Antoine Sireyjol <antoine.sireyjol@gmail.com>
2019-02-19 16:42:26 +0100

    dernieres modifs

--- mon_projet/code R/mon_code.R
+++ mon_projet/code R/mon_code.R
@@ -1,4 +1,6 @@
-df <- tibble(a = 1:100, b = sample(50), c = rnorm(100))
-df %>% group_by(b) %>% summarise(moy = mean(c)) -> out
+library(dplyr)
 
-write.csv(out, file = "output/moyenne.csv")
+df <- data.frame(a = c(1:100), b = sample(50), c = rnorm(100), replace = TRUE)
+df %>% as_tibble() %>% group_by(b) %>% summarise(moy = mean(c)) -> out
+
+write.table(out, file = "output/moyenne.txt")
